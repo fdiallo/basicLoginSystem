e16dad6480776c91efe57d43adb84eb6b34d29f6
fdiallo <fallaye.diallo@gmail.com>
2026-05-20 23:38:14 -0400

    Task 1: Project Setupd

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require("express")
+const app = express()
+require("dotenv").config()
+
+const userRouter = require("./userRoutes.js")
+
+const { connectDB } = require("./connection.js")
+
+connectDB()
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())     // Middleware to parse JSON bodies
+
+// Routes
+app.use("/", userRouter);   // Mount the router
+
+// Port
+const PORT = process.env.PORT
+app.listen(PORT, () => { console.log(`Server listening on Port: ${PORT}`) })
---
+++ userRoutes.js
@@ -0,0 +1,10 @@
+// Dependencies
+const express = require("express")
+const router = express.Router()
+const Product = require("./User.js")
+
+
+
+
+module.exports = router
+
