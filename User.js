4aaa7a4bf2f3ee050bdd5006f7e71fbc64ba95c3
fdiallo <fallaye.diallo@gmail.com>
2026-05-21 00:08:03 -0400

    Task 2: Build the Registration Endpoint

--- User.js
+++ User.js
@@ -1,25 +1,49 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
  
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 8, },
+});
+ 
+// Set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+ 
+  next();
 });
  
 const User = model("User", userSchema);
  
 export default User;
+
+
+// import { Schema, model } from "mongoose";
+ 
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     match: [/.+@.+\..+/, "Must match an email address!"],
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 8,
+//   },
+// });
+ 
+// const User = model("User", userSchema);
+ 
+// export default User;
--- userRoutes.js
+++ userRoutes.js
@@ -1,10 +1,29 @@
-// Dependencies
-const express = require("express")
-const router = express.Router()
-const Product = require("./User.js")
+const express = require('express');
+const router = express.Router();
+const User = require("./User.js");
 
+// Create a POST route (e.g., /api/users/register)
+router.post("/api/users/register", async (req, res) => {
+    try {
+        //Take the username, email, and password from the req.body
+        const { username, email, password } = req.body;
 
+        // Check if user already exists. If so, return a 400 status
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists with this email." });
+        }
 
+        // Create new user, if the user does not exist
+        const newUser = new User({ username, email, password });
+        
+        // Save to DB (triggers the pre-save hook)
+        await newUser.save();
 
-module.exports = router
+        res.status(201).json(userResponse);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+});
 
+module.exports = router;
